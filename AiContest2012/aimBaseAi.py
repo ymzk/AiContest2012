33a5c88cc222a0a4894df3fd91c25a54fd80175b
upa <okumura.upa@gmail.com>
2012-05-19 22:25:49 +0900

    add collectItemAi

--- AiContest2012/aimBaseAi.py
+++ AiContest2012/aimBaseAi.py
@@ -5,11 +5,21 @@ class AimBaseAi(AiInterface):
   def initCalculation(self):
     self.move = None
   def send(self):
-    opponent = self.bases[1 - self.myunit.team]
-    if (self.myunit.position[0] - opponent.position[0]) ** 2 + (self.myunit.position[1] - opponent.position[1]) ** 2 < 360000:
-      if self.canShoot(self.myunit.position, opponent.position):
-        self.sendData(0,self.regularizeAngle(atan2(opponent.position[1] - self.myunit.position[1], opponent.position[0] - self.myunit.position[0]) - self.myunit.direction),firing = True)
-        return
+    for i in self.units:
+      if i.team == self.myunit.team:
+        continue
+      else:
+        if self.canShoot(self.myunit.position, i.position):
+          angle = atan2(i.position[1] - self.myunit.position[1], i.position[0] - self.myunit.position[0])
+          self.sendData(speed = 3, angle = self.regularizeAngle(angle - self.myunit.direction),firing = True)
+          return
+    for opponent in self.bases:
+      if opponent.team == self.myunit.team:
+        continue
+      if (self.myunit.position[0] - opponent.position[0]) ** 2 + (self.myunit.position[1] - opponent.position[1]) ** 2 < 360000:
+        if self.canShoot(self.myunit.position, opponent.position):
+          self.sendData(0,self.regularizeAngle(atan2(opponent.position[1] - self.myunit.position[1], opponent.position[0] - self.myunit.position[0]) - self.myunit.direction),firing = True)
+          return
     if self.move == None:
       self.move = MoveTo(self.field, self.myunit, self.bases[1 - self.myunit.team].position)
     self.sendData(*self.move.get(self.field, self.myunit))
---
+++ AiContest2012/collectItemAi.py
@@ -0,0 +1,45 @@
+from aiInterface import AiInterface
+from aiLibrary.moveTo import MoveTo
+from math import atan2
+class CollectItemAi(AiInterface):
+  def initCalculation(self):
+    self.baseMove = None
+    for w in range(self.field.width):
+      for h in range(self.field.height):
+        if self.field.fieldData[w][h] == -1:
+          self.itemMove = MoveTo(self.field, self.myunit, ((w + 0.5) * self.field.cellWidth, (h + 0.5) * self.field.cellHeight))
+          return
+  def send(self):
+    if self.myunit.attack <= 10:
+      for i in self.units:
+        if i.team == self.myunit.team:
+          continue
+        else:
+          angle = atan2(i.position[1] - self.myunit.position[1], i.position[0] - self.myunit.position[0])
+          self.sendData(speed = 3, angle = self.regularizeAngle(angle - self.myunit.direction),firing = True)
+          return
+      self.sendData(*self.itemMove.get(self.field, self.myunit))
+      return
+    for opponent in self.bases:
+      if opponent.team == self.myunit.team:
+        continue
+      if (self.myunit.position[0] - opponent.position[0]) ** 2 + (self.myunit.position[1] - opponent.position[1]) ** 2 < 360000:
+        if self.canShoot(self.myunit.position, opponent.position):
+          self.sendData(3,self.regularizeAngle(atan2(opponent.position[1] - self.myunit.position[1], opponent.position[0] - self.myunit.position[0]) - self.myunit.direction),firing = True)
+          return
+    for i in self.units:
+      if i.team == self.myunit.team:
+        continue
+      else:
+        angle = atan2(i.position[1] - self.myunit.position[1], i.position[0] - self.myunit.position[0])
+        self.sendData(speed = 3, angle = self.regularizeAngle(angle - self.myunit.direction),firing = True)
+        return
+    if self.baseMove == None:
+      self.baseMove = MoveTo(self.field, self.myunit, self.bases[1 - self.myunit.team].position)
+    self.sendData(*self.baseMove.get(self.field, self.myunit))
+    return
+          
+if __name__ == "__main__":
+  ai = CollectItemAi()
+  #ai.run(open("initMessage","r"),open("message","r"))
+  ai.run()
